abfb2dbfa2e64d96f0fbe3c5c63551414a0062f7
Alleluid <alleluid@users.noreply.github.com>
2018-07-05 02:00:55 -0700

    implemented some rolling tests

--- game/dice_roll.py
+++ game/dice_roll.py
@@ -1,18 +1,20 @@
 import random
+from collections import Counter
 
 
-def roll(die, count, roll_list=False):
+def roll(count, die, roll_list=False):
     rolls = []
     for i in range(count):
         rolls.append(random.randint(1, die))
     if roll_list:
-        return sum(rolls), rolls
+        return rolls
     else:
         return sum(rolls)
 
 
 def testing():
-    pass
+    print("3d6:", Counter([roll(3, 6) for i in range(100_000)]))
+    print("1d20:", Counter([roll(1, 20) for i in range(100_000)]))
 
 
 if __name__ == '__main__':
