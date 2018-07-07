362a6031115c9462fd87419b5bc0fb1a446e9c94
Alleluid <alleluid@users.noreply.github.com>
2018-07-07 13:40:47 -0700

    renamed dir/package for game

--- adventure/game.py
+++ adventure/game.py
@@ -1,5 +1,6 @@
-from game.dice_roll import roll
+from adventure.dice_roll import roll
 import collections
+import socket
 
 
 class TextGame:
@@ -10,6 +11,15 @@ class TextGame:
         def __init__(self, pow=10):
             pass
 
+    #####
     def __init__(self, char_name):
         self.character = char_name
         self.stats = self.Stats()
+
+
+def sockets(host_tup=None):
+    if not host_tup:
+        host_tup = ("127.0.0.1", 5000)
+    with socket.socket() as sock:
+        sock.connect(host_tup)
+        sock.send(b"my data")
---
+++ adventure/run_cmd.py
@@ -0,0 +1,4 @@
+# Workaround file since asciimatics doesn't work within PyCharm
+import os
+
+os.system('start "C:\\Users\\milot\\PycharmProjects\\pythonsandbox\\venv37\\Scripts\\python.exe" C:\\Users\\milot\\PycharmProjects\\pythonsandbox\\game\\interface.py')
