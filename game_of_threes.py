c68b4112650c4c4854fa80e19cf7b21603d6f670
Alleluid <alleluid@users.noreply.github.com>
2018-09-14 16:44:53 -0700

    Reworked game of threes main function

--- game_of_threes.py
+++ game_of_threes.py
@@ -1,5 +1,17 @@
 def play(num):
+    while num > 1:
+        div, mod = divmod(num, 3)
+        if mod == 0:
+            print(num, '0')
+            num = div
+        elif mod == 1:
+            print(num, '-1')
+            num = (num - 1) / 3
+        elif mod == 2:
+            print(num, '+1')
+            num = (num + 1) / 3
 
+    print('1')
 
 
 def testing():
